529b27e3f89854f67d4be228271185e66d528454
Nibbler <kwimbish@hotmail.com>
2022-04-23 17:51:58 -0500

    app.get/post/use functions

--- app.js
+++ app.js
@@ -1,28 +1,83 @@
 const express = require('express');
 const path = require('path');
+const allNote = require('/db/db.json');
 
 // to initialize
 const app = express();
-
 // to assign the port
 const port = 3001;
-
 // to import
 const fs = require('fs');
 
-//API routes (get, post, delete) for the notes
-app.get('/api', (req, res) => {
-
-});
+// make sure to use these!!! https://expressjs.com/en/api.html app.use, express.urlencoded, express.json, express.static.
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
 
 // HTML routes, to get the html to link and work...
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'))
+  res.sendFile(path.join(__dirname, './public/notes.html'))
 });
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'))
+  res.sendFile(path.join(__dirname, './public/index.html'))
 });
 
-// to link the port so everything works...
+// API get
+app.get('/api/notes', (req, res) => {
+  res.json(allNote.slice(1));
+});
+// end get
+
+//API route - post/new
+function createNew(body, noteArray) {
+  const newNote = body;
+  if (!Array.isArray(noteArray))
+    noteArray = [];
+  if (noteArray.length === 0)
+    noteArray.push(0);
+  body.id = noteArray[0];
+  noteArray[0]++;
+
+  noteArray.push(newNote);
+  fs.writeFileSync(path.join(__dirname, '/db/db.json'),
+    JSON.stringify(noteArray, null, 2)
+  );
+  return newNote;
+};
+
+app.post('/api/notes', (req, res) => {
+  const newNote = createNew(req.body, allNote);
+  res.json(newNote);
+});
+// end post/new 
+
+//API route - delete
+function deleteNote(id, noteArray) {
+  for (let i = 0; i < noteArray.length; i++) {
+    let note = noteArray[i];
+
+    if (note.id == id) {
+      noteArray.splice(i, 1);
+      fs.writeFileSync(
+        path.join(__dirname, '/db/db.json'),
+        JSON.stringify(noteArray, null, 2)
+      );
+
+      break;
+    }
+  }
+};
+
+app.get('/api/notes/:id', (req, res) => {
+    deleteNote(req.params.id, allNote);
+    res.json(true);
+});
+// end delete
+
+// to link/listen to the port...
 app.listen(port, () => console.log(`Listening on port ${port}`));
